fea426101086eab55c88280d1c92c46285c75bae
Nadia Tsygankova <nadiapritchina@gmail.com>
2023-07-15 12:24:03 -0400

    new pages classnames

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,10 +1,17 @@
-
+//library for sum classes
+import classnames from "classnames"
 
 function Button({ children, size, className, ...rest }) {
+  let sizeClass = size ? `button-${size}`: ""
+  const allClasses = classnames(sizeClass, className)
 
   return (
     <div>
-      <button  {...rest} className = {`${className} ${(size==='lg')?'button-large':'button-small'}`}>
+      <button  {...rest}
+      //my solution without library
+      // className = {`${className} ${(size==='lg')?'button-large':'button-small'}`}
+      className = {allClasses}
+      >
         {children}
       </button>
     </div>
